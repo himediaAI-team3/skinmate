e1c659584668b0f18237de1ee1a0ee87449226f1
Choheeeee <145943067+Choheeeee@users.noreply.github.com>
2025-10-23 15:09:02 +0900

    Refactor: DB 스키마 수정에 따른 모델 수정
    - ENUM 정의 (File -  entity_type )
    - File/Cosmetic 모델 수정

--- backend/app/models/file.py
+++ backend/app/models/file.py
@@ -1,12 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Enum as SQLEnum
 from .common import Base, Common
+from .entity_type import EntityType
 
 class File(Base, Common):
     __tablename__ = "file"
 
     file_id = Column(Integer, primary_key=True, autoincrement=True)
-    analysis_id = Column(Integer, nullable=True)
-    file_url = Column(String(255), nullable=True)
+    entity_type = Column(SQLEnum(EntityType), nullable=True)
+    entity_id = Column(Integer, nullable=True)
+    file_path = Column(String(255), nullable=True)
     file_name = Column(String(255), nullable=True)
     mime_type = Column(String(100), nullable=True)
     size = Column(Integer, nullable=True)
